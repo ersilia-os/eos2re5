a1e4298ee02a1aff11e1783856f71abab58c2c3a
Samuel Volk <svolk19@stanford.edu>
2022-08-25 13:40:27 -0700

    Added models requiring ChemoPy descriptors

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 - Tags: ADMET, bioactivity, toxicity
 
 # Model description
-A series of models for the systematic ADMET evaluation of drug candidate molecules. Models include blood-brain barrier penetration; inhibition and substrate affinity for CYP1A2, CYP2C9, CYP2C19, CYP2D6, CYP3A4, and pgp; F 20% and F 30% bioavailability; human intestinal absorption; Ames mutagenicity; and skin sensitization. 
+A series of models for the systematic ADMET evaluation of drug candidate molecules. Models include blood-brain barrier penetration; inhibition and substrate affinity for CYP1A2, CYP2C9, CYP2C19, CYP2D6, CYP3A4, and pgp; F 20% and F 30% bioavailability; human intestinal absorption; Ames mutagenicity; skin sensitization; plasma protein binding; volume distribution; LD50 of acute toxicity; human hepatotoxicity; hERG blocking; clearance; half-life; Papp (caco-2 permeability); LogD distribution coefficient; and LogS solubility. 
 
 - Input: SMILES 
-- Output: A vector of model classification outputs. Each model contributes two values to the vector: a binary label value (e.g. whether a molecule is a CYP1A2 inhibitor or not) and float probability value (e.g. the probability of a molecule acting as a CYP1A2 inhibitor). All label thresholds are at p=50%. 
-- Model type: Classification
+- Output: A vector of classification and regression outputs. Each classification model contributes two values to the vector: a binary label value <model name>_label (e.g. whether a molecule is a CYP1A2 inhibitor or not) and float probability value <model name>_prob (e.g. the probability of a molecule acting as a CYP1A2 inhibitor). All classification label thresholds are at p=50%. Each regression model contributes one prediction float value <model name>_pred to the vector. The numerical ranges and meanings of the outputs are described in detail on the [ADMETlab website](https://admet.scbdd.com/home/interpretation/) and in the [ADMETlab publication](https://jcheminf.biomedcentral.com/articles/10.1186/s13321-018-0283-x).
+- Model type: Classification and regression
 - Training set: Training datasets for each model are documented on the ADMETlab website [here](https://admet.scbdd.com/home/interpretation/). 
 - Mode of training: Pretrained. Model checkpoints were downloaded from the [GitHub repo](https://github.com/ifyoungnet/ADMETlab) provided by ADMETlab.
 
@@ -18,12 +18,15 @@ Jie Dong, Ning-Ning Wang, Zhi-Jiang Yao, Lin Zhang, Yan Cheng, Defang Ouyang, Ai
 - Code: https://github.com/ifyoungnet/ADMETlab/tree/master/example
 - Checkpoints: https://github.com/ifyoungnet/ADMETlab
 
+The plasma protein binding, volume distribution, clearance, half-life, hERG blocking, LD50 of acute toxicity, Papp, logD, and logS models take chemical descriptors as inputs. These descriptors are calculated using the no longer maintained ChemoPy (pychem) library archived [here](https://code.google.com/archive/p/pychem/source) and originally downloaded for use from [this repository](https://github.com/salotz/chemopy). 
+
 # License
 The GPL-v3 license applies to all parts of the repository that are not externally maintained libraries. This repository uses the externally maintained library "ADMETlab", located at [/model/framework](https://github.com/ersilia-os/eos2re5/tree/main/model/framework/ADMETlab) and licensed under a [GNU Affero General Public License](https://github.com/ersilia-os/eos2re5/blob/main/model/framework/ADMETlab/LICENSE).
 
 # History 
 - Model checkpoints were downloaded from https://github.com/ifyoungnet/ADMETlab on 7/28/2022.
 - No modifications were made to the original code, though the code in [/example/run.py](https://github.com/ersilia-os/eos2re5/tree/main/model/framework/ADMETlab/example/run.py) was copied and adapted for use in [/code/python_2_main.py](https://github.com/ersilia-os/eos2re5/blob/main/model/framework/code/python_2_main.py)
+- The ChemoPy chemical descriptor library was downloaded from https://github.com/salotz/chemopy on 8/24/2022. Each import reference to the legacy pybel library in the ChemoPy source code was changed to instead import pybel from the openbabel library.
 
 # About us
 The [Ersilia Open Source Initiative](https://ersilia.io) is a Non Profit Organization ([1192266](https://register-of-charities.charitycommission.gov.uk/charity-search/-/charity-details/5170657/full-print)) with the mission is to equip labs, universities and clinics in LMIC with AI/ML tools for infectious disease research.
--- model/framework/code/python_2_main.py
+++ model/framework/code/python_2_main.py
@@ -5,15 +5,23 @@ Created on Tues Jul 26 2022
 
 @author: Samuel Volk
 """
+# point to admetlab/chemopy paths
+import os, sys
+path_root = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..")
+sys.path.append(os.path.abspath(path_root))
+
+# additional imports
 from sklearn.externals import joblib
+import ast
 import numpy as np
 import pandas as pd
+from chemopy.src.pychem import constitution, topology, moran, moe, \
+    molproperty, connectivity, bcut, estate, basak, moreaubroto
+from chemopy.src.pychem.pychem import PyChem2d
 from rdkit.Chem import DataStructs
 from rdkit.Chem import rdMolDescriptors
 from rdkit.Chem import AllChem
 from rdkit import Chem
-import sys
-import os
 
 # parse arguments
 input_file = sys.argv[1]
@@ -25,11 +33,6 @@ file_path = os.path.dirname(os.path.abspath(__file__))
 # hide gpus
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 
-# point to the admetlab paths
-path_root = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "..")
-sys.path.append(os.path.abspath(path_root))
-sys.path.append(os.path.join(path_root, "checkpoints", "classification_model"))
-
 # configure relevant resource paths
 checkpoints_dir = os.path.abspath(os.path.join(file_path, "..", "..", "checkpoints"))
 framework_dir = os.path.abspath(os.path.join(file_path, ".."))
@@ -39,6 +42,28 @@ smiles_df = pd.read_csv(input_file)
 smiles_list = smiles_df[smiles_df.columns[0]].tolist()
 model_meta = pd.read_csv(os.path.join(framework_dir, 'model_metadata.csv'))
 
+# function to parse descriptor list needed for some models and assemble input
+def smiles_to_descriptors(smiles_list, descriptor_list):
+    df = pd.DataFrame(columns=descriptor_list)
+    pc2D = PyChem2d()
+    for i in smiles_list:
+        mol_rdkit = Chem.MolFromSmiles(i)
+        pc2D.ReadMolFromSmile(i)
+        des = constitution.GetConstitutional(mol_rdkit)
+        des.update(moe.GetMOE(mol_rdkit)) # concat new descriptors
+        des.update(topology.GetTopology(mol_rdkit))
+        des.update(connectivity.GetConnectivity(mol_rdkit))
+        des.update(moran.GetMoranAuto(mol_rdkit))
+        des.update(moreaubroto.GetMoreauBrotoAuto(mol_rdkit))
+        des.update(molproperty.GetMolecularProperty(mol_rdkit))
+        des.update(estate.GetEstate(mol_rdkit))
+        des.update(bcut.GetBurden(mol_rdkit))
+        des.update(basak.Getbasak(mol_rdkit))
+        des.update(pc2D.GetCharge())
+        des.update(pc2D.GetKappa())
+        df.loc[len(df)] = [des[j] for j in descriptor_list] 
+    return df.to_numpy()   
+ 
 # function for creating ECFP descriptors from SMILES strings
 def smiles_to_ECFP(smiles_list, diameter = 4, numBits = 1024):
     df = pd.DataFrame(columns=[str(i) for i in range(numBits)])
@@ -63,17 +88,32 @@ def smiles_to_MACCS(smiles_list):
 
 # load models and complete prediction for each
 outcome_df = pd.DataFrame({'smiles': smiles_list})
-current_path = os.path.split(os.path.realpath(__file__))[0]
+is_not_ECFP = model_meta['ECFP_type'].isnull().to_numpy()
+is_not_MACCS = model_meta['MACCS'].isnull().to_numpy()
 for index, row in model_meta.iterrows():
-    cf = joblib.load(checkpoints_dir + "/classification_model" + row['path'])
-    if row['MACCS'] == 1:
-        FPs = smiles_to_MACCS(smiles_list)
-    else:
-        FPs = smiles_to_ECFP(smiles_list, int(row['ECFP_type']), int(row['ECFP_nbits']))
-    y_hat_label = cf.predict(FPs)
-    y_predict_proba = cf.predict_proba(FPs)
-    outcome_df[row['name'] + '_label'] = y_hat_label
-    outcome_df[row['name'] + '_prob'] = [i[1] for i in y_predict_proba]
+    # load model checkpoint
+    cf = joblib.load(checkpoints_dir + row['path'])
+
+    # form input based on model input type
+    if is_not_MACCS[index] and is_not_ECFP[index]: # model takes chemical descriptors as input
+        descriptor_list = ast.literal_eval(row['descriptors'])
+        input = smiles_to_descriptors(smiles_list, descriptor_list)
+       
+    elif is_not_MACCS[index]: # model takes ECFP as input
+        input = smiles_to_ECFP(smiles_list, int(row['ECFP_type']), int(row['ECFP_nbits']))
+    
+    else: # model takes MACCS as input
+        input = smiles_to_MACCS(smiles_list)
+
+    # generate predictions based on model type (regression/classification)
+    if row['model_type'] == "classification": # classification model
+        y_hat = cf.predict(input)
+        y_proba = cf.predict_proba(input)
+        outcome_df[row['name'] + '_label'] = y_hat
+        outcome_df[row['name'] + '_prob'] = [i[1] for i in y_proba]
+    else: # regression model
+        y_pred = cf.predict(input)
+        outcome_df[row['name'] + '_pred'] = y_pred
 
 # write output to csv
 outcome_df.to_csv(output_file, index=False)
