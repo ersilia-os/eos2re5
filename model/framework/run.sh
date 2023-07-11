70105dd152a6e1f7c277d8028fe6a5410294efdb
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2023-07-11 14:30:23 +0200

    Update run.sh to work inside docker

--- model/framework/run.sh
+++ model/framework/run.sh
@@ -1,4 +1,4 @@
 source $CONDA_PREFIX_1/etc/profile.d/conda.sh
-conda activate eos2re5-py27
-python $1/code/main.py $2 $3
-conda deactivate
+CONDA_PATH=$(dirname $(dirname $(which conda)))
+PYTHON_ENV_PATH="${CONDA_PATH}/envs/eos2re5-py27/bin/python"
+$PYTHON_ENV_PATH $1/code/main.py $2 $3
